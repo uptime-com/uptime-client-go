09ea82f5f94aa84955392784c730ee5c69813519
Mikhail Lukianchenko <42915+mikluko@users.noreply.github.com>
2023-03-21 18:23:54 +0100

    Use string builder for error message generation

--- pkg/upapi/error.go
+++ pkg/upapi/error.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+	"strings"
 )
 
 var DecodeError = errors.New("error response decode error")
@@ -25,22 +26,17 @@ func NewError() *Error {
 }
 
 func (e Error) Error() string {
-	if e.Fields == nil {
-		return fmt.Sprintf("%s %s failed: Code=%v Message=%s",
-			e.Response.Request.Method,
-			e.Response.Request.URL.String(),
-			e.Code,
-			e.Message,
-		)
-	} else {
-		return fmt.Sprintf("%s %s failed: Code=%v Message=%s, Fields=%v",
-			e.Response.Request.Method,
-			e.Response.Request.URL.String(),
-			e.Code,
-			e.Message,
-			e.Fields,
-		)
+	b := strings.Builder{}
+	b.WriteString(fmt.Sprintf("%s %s failed: Code=%v Message=%s",
+		e.Response.Request.Method,
+		e.Response.Request.URL.String(),
+		e.Code,
+		e.Message,
+	))
+	if e.Fields != nil {
+		b.WriteString(fmt.Sprintf(", Fields=%v", e.Fields))
 	}
+	return b.String()
 }
 
 func ErrorFromResponse(r *http.Response) error {
