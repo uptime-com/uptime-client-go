ed5e3162268d60de81880617695682d2322112d3
Mikhail Lukianchenko <42915+mikluko@users.noreply.github.com>
2023-11-08 16:32:44 +0100

    Fix uptime SLA field type on WHOIS check

--- pkg/upapi/ep_checks.go
+++ pkg/upapi/ep_checks.go
@@ -972,17 +972,17 @@ func (c checksEndpointWebhookImpl) UpdateWebhook(ctx context.Context, pk Primary
 }
 
 type CheckWHOIS struct {
-	Name          string   `json:"name,omitempty"`
-	ContactGroups []string `json:"contact_groups,omitempty"`
-	Locations     []string `json:"locations,omitempty"`
-	Tags          []string `json:"tags,omitempty"`
-	IsPaused      bool     `json:"is_paused"`
-	Address       string   `json:"msp_address"`
-	ExpectString  string   `json:"msp_expect_string,omitempty"`
-	Threshold     int64    `json:"msp_threshold,omitempty"`
-	NumRetries    int64    `json:"msp_num_retries,omitempty"`
-	UptimeSla     float64  `json:"msp_uptime_sla,omitempty"`
-	Notes         string   `json:"msp_notes,omitempty"`
+	Name          string          `json:"name,omitempty"`
+	ContactGroups []string        `json:"contact_groups,omitempty"`
+	Locations     []string        `json:"locations,omitempty"`
+	Tags          []string        `json:"tags,omitempty"`
+	IsPaused      bool            `json:"is_paused"`
+	Address       string          `json:"msp_address"`
+	ExpectString  string          `json:"msp_expect_string,omitempty"`
+	Threshold     int64           `json:"msp_threshold,omitempty"`
+	NumRetries    int64           `json:"msp_num_retries,omitempty"`
+	UptimeSLA     decimal.Decimal `json:"msp_uptime_sla,omitempty"`
+	Notes         string          `json:"msp_notes,omitempty"`
 }
 
 type checksEndpointWHOISImpl struct {
